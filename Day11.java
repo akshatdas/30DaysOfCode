7c5f794f5b4cbdf00493552d93eebd4903f905a2
akshatdas <akshat.das@berkeley.edu>
2017-01-02 00:47:08 -0800

    Fixed formatting of Day 11

--- Day11.java
+++ Day11.java
@@ -21,8 +21,8 @@ public class Solution {
         	for (int j = 0; j < 4; j++) {
         		// Go through all potential hourglass shapes
         		hourglassSum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-        					 +		       arr[i+1][j+1]
-        					 + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+        					 + arr[i+1][j+1]
+        				+ arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
 
         		// Reset max value if new max is found
         		if (hourglassSum > maxHourglassSum) {
